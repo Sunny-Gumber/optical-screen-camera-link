facb35718776ee86cb49563b572bba1122c03e4d
Sunny-Gumber <68242840+Sunny-Gumber@users.noreply.github.com>
2026-08-31 16:36:47 +0530

    Export S8C16 robust frame codec

--- packages/optical-codec/src/index.js
+++ packages/optical-codec/src/index.js
@@ -2,6 +2,7 @@ export * from './profiles.js';
 export * from './symbol-codec.js';
 export * from './frame.js';
 export * from './v2-frame.js';
+export * from './v21-frame.js';
 export * from './fiducials.js';
 export * from './shapes.js';
 export * from './svg-renderer.js';
